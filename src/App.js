0aa7c7aa65c04a26a26eb6401db7c4805ed594d2
Justin Sterling <justinsterling44@gmail.com>
2023-11-09 15:00:35 -0500

    Adding code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+/**
+ * @function Markdown Lets the user enter markdown text into a textarea,
+ * displays parsed text in preview area
+ * 
+ * @param {String} text The initial markdown string
+ * @param {String} content Holds the markdown string
+ * 
+ * @method setContent Sets the value of the markdown string
+ */
+function Markdown() {
+  let text = `
+# h1
+
+## h2
+
+[link](#)
+
+\` inline \`
+
+\`\`\`
+block
+\`\`\`
+
+- li
+
+> blockquote
+
+![image](image.jpg)
+
+**bold**
+`;
+  const [content, setContent] = useState(text);
+ /**
+ * @function window.onload On window load send parsed initial markdown 
+ * text to preview element
+ */
+  window.onload = function() {
+    document.getElementById('preview').innerHTML =
+      marked.parse(text);
+  }
+  /**
+  * @function handleChange On change in text area sends parsed markdown
+  * text to preview element
+  * 
+  * @param {Element} event Current target element
+  */
+  function handleChange(event) {
+    setContent(event.target.value);
+    document.getElementById("preview").innerHTML = marked.parse(event.target.value);
+  };
+  
+  return (
+    <div id="wrap-box">
+      <textarea value={content} onChange={handleChange} id="editor" rows="3" cols="40" ></textarea>
+      <div id="preview">
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Markdown />
+
       </header>
     </div>
   );
